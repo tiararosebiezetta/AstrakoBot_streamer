6d1259f1a0d31d21f8c0a417990001c230e0f4e6
Tiararose Biezetta <92842340+tiararosebiezetta@users.noreply.github.com>
2021-11-28 15:38:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
-## Clone from upstream repo
-rm -rf .git
-git clone $UPSTREAM_REPO tmp
-mv tmp/.git .
-rm -rf tmp
-git reset --hard
-
 ## Get config
-wget -q $CONFIG_URL -O AstrakoBot/config.py
+wget -q $CONFIG_URL -O config.ini
 
 ## Run the bot
-python3 -m AstrakoBot
+python3 -m tg_bot
